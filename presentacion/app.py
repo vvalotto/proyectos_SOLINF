95269285a91a71f126d26e58a1154c9d8021506b
Victor Valotto <vvalotto@gmail.com>
2019-10-20 18:57:25 -0300

    Inicio de Flask - Uso de Templates

--- presentacion/app.py
+++ presentacion/app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+
+
 @app.route("/")
 def index():
-    return ("Hola Mundo!")
+    user = {'nombre': 'Python'}
+    return render_template("index.html", usuario=user)
 
 
 @app.route("/suma")
@@ -15,6 +26,8 @@ def suma():
 
 @app.route("/mayuscula/<string:palabra>")
 def mayuscula(palabra):
+    if len(palabra) < 2:
+        return render_template("500.html")
     return str(palabra).upper()
 
 
